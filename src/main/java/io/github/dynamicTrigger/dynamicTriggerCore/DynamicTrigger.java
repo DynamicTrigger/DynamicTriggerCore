6d3841445454d2b0b2c779344cfb6ec25439c1ff
HeartPattern <jun2620354@naver.com>
2016-05-04 16:39:08 +0900

    ReInit

---
+++ src/main/java/io/github/dynamicTrigger/dynamicTriggerCore/DynamicTrigger.java
@@ -0,0 +1,10 @@
+package io.github.dynamicTrigger.dynamicTriggerCore;
+
+/**
+ * Created by jun26 on 2016-05-04.
+ */
+public class DynamicTrigger {
+    public static void main(String... args) {
+
+    }
+}
